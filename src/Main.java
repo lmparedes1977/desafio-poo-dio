066c2269b57efcf396afc471f78dc8d06b5f8edf
lmparedes1977 <lmparedes@gmail.com>
2023-03-16 11:36:31 -0300

    Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("java");
+        curso1.setDescricao("Java 17 na DIO");
+        curso1.setCargaHoraria(180);
+
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("javascript");
+        curso1.setDescricao("Javascript na DIO");
+        curso1.setCargaHoraria(180);
+
+        System.out.println(curso2);
+
+        Mentoria mento1 = new Mentoria();
+        mento1.setNome("Java 18");
+        mento1.setDescricao("Fazendo upgrade do Java");
+        mento1.setData(LocalDate.now());
+
+        System.out.println(mento1);
+
+        
+
     }
 }
---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,59 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+
+    private String nome;
+    private String descricao;
+    private LocalDate data;
+
+    
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return String return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return LocalDate return the data
+     */
+    public LocalDate getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String toString(){
+        return "nome=" + this.nome + "; descricao=" + this.descricao + "; data=" + this.data;
+    }
+
+}
